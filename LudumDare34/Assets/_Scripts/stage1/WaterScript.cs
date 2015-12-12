537fc38f6abd73456ea7a3b9f02d49642b8dbfb0
Zegram <Joikkiz@hotmail.com>
2015-12-12 22:38:10 +0200

    Stage 1 Added

---
+++ LudumDare34/Assets/_Scripts/stage1/WaterScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterScript : MonoBehaviour 
+{
+    public GUIStyle progress_empty;
+    public GUIStyle progress_full;
+
+    public float waterAmount;
+    //current progress
+    public float barDisplay;
+
+    public Vector2 pos = new Vector2(10, 50);
+    Vector2 size = new Vector2(100, 50);
+
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+
+    void OnGUI()
+    {
+        //draw the background:
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y), emptyTex, progress_empty);
+
+        GUI.Box(new Rect(pos.x, pos.y, size.x, size.y), fullTex, progress_full);
+
+        //draw the filled-in part:
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), fullTex, progress_full);
+
+        GUI.EndGroup(); 
+        GUI.EndGroup();
+    }
+
+    public void AddWater()
+    {
+        waterAmount += 25.0f;
+        if (waterAmount > 100)
+            waterAmount = 100;
+    }
+
+    void Update()
+    {
+        waterAmount -= 0.25f;
+        if (waterAmount < 0)
+            waterAmount = 0;
+        //the player's health
+        barDisplay = waterAmount / 100;
+    }
+}
---
+++ LudumDare34/Assets/_Scripts/stage1/witheringScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class witheringScript : MonoBehaviour
+{
+
+    public float withering;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        withering = transform.FindChild("Water Bar").GetComponent<WaterScript>().waterAmount;
+        withering += transform.FindChild("Nutrient Bar").GetComponent<WaterScript>().waterAmount;
+        var color = transform.GetComponent<SpriteRenderer>().color;
+        color.a = (withering / 200);
+        transform.GetComponent<SpriteRenderer>().color = color;
+    }
+}
