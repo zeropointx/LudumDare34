1906f02b929f4605e0206a7183610362d72f26fb
Zegram <Joikkiz@hotmail.com>
2015-12-12 22:25:58 +0200

    Apple grabber mechanic working, everything else is placeholders.

---
+++ LudumDare34/Assets/_Scripts/Stage 3/grapperScript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grapperScript : MonoBehaviour
+{
+    Vector3 position;
+    public GameObject apple;
+    public int minY, maxY, minX, maxX;
+    bool hPlus, hMinus, vPlus, vMinus;
+    public bool horizontalMode, verticalMode;
+    public GameObject rotateButton, grabButton;
+    public int speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        horizontalMode = true;
+        verticalMode = false;
+        hPlus = false;
+        hMinus = false;
+        vPlus = false;
+        vMinus = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = transform.position;
+
+        checkCollision();
+        move();
+
+
+    }
+
+    void checkCollision()
+    {
+        if (horizontalMode)
+        {
+            //vPlus = false;
+            //vMinus = false;
+
+            if (position.x >= maxX)
+            {
+                hMinus = true;
+                hPlus = false;
+            }
+            if (position.x <= minX)
+            {
+                hMinus = false;
+                hPlus = true;
+
+            }
+        }
+        else
+        {
+            //hPlus = false;
+            //hMinus = false;
+
+            if (position.y >= maxY)
+            {
+                vMinus = true;
+                vPlus = false;
+            }
+            if (position.y <= minY)
+            {
+                vMinus = false;
+                vPlus = true;
+            }
+        }
+    }
+
+    void move()
+    {
+        if (horizontalMode)
+        {
+            if (hPlus)
+                transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+
+            if (hMinus)
+                transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+
+        if (verticalMode)
+        {
+            if (vPlus)
+                transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+
+            if (vMinus)
+                transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+
+        }
+
+}
+
+
---
+++ LudumDare34/Assets/_Scripts/Stage 3/rotateButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateButtonScript : MonoBehaviour {
+
+    grapperScript GrapperScript;
+	// Use this for initialization
+	void Start () 
+    {
+        GameObject Grapper = GameObject.Find("grapper");
+        GrapperScript = Grapper.GetComponent<grapperScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        if(GrapperScript.horizontalMode)
+        {
+            GrapperScript.horizontalMode = false;
+            GrapperScript.verticalMode = true;
+        }
+        
+        else if(GrapperScript.verticalMode)
+        {
+            GrapperScript.horizontalMode = true;
+            GrapperScript.verticalMode = false;
+        }
+    }
+}
