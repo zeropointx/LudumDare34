395464650fc7644131aac677a4815541cba49875
zeropointx <tti13smiikas@kamk.fi>
2015-12-12 16:49:42 +0200

    Added main menu and scenes for eeeverybody

---
+++ LudumDare34/Assets/_Scripts/ChangeStage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeStage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void ChangeScene(int number)
+    {
+        Application.LoadLevel("Stage" + number);
+    }
+}
