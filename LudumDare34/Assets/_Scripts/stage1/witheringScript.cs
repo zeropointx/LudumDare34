1eb88ba8cd577436eee417ea82a879a6fe191dc3
Zegram <Joikkiz@hotmail.com>
2015-12-13 23:17:03 +0200

    Stage1 fixes

--- LudumDare34/Assets/_Scripts/stage1/witheringScript.cs
+++ LudumDare34/Assets/_Scripts/stage1/witheringScript.cs
@@ -15,9 +15,13 @@ public class witheringScript : MonoBehaviour
     void Update()
     {
         withering = transform.FindChild("Water Bar").GetComponent<WaterScript>().waterAmount;
-        withering += transform.FindChild("Nutrient Bar").GetComponent<WaterScript>().waterAmount;
-        var color = transform.GetComponent<SpriteRenderer>().color;
+        withering += transform.FindChild("Nutrient Bar").GetComponent<NutrientScript>().waterAmount;
+        Color color = transform.GetComponent<SpriteRenderer>().color;
         color.a = (withering / 200);
         transform.GetComponent<SpriteRenderer>().color = color;
+        if(withering <= 0)
+        {
+            GlobalVariables.globalVariables.addHp(-1);
+        }
     }
 }
