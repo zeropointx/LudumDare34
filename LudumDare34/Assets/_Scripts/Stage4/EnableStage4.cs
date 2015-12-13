5d3157b6442807c3b97eecb0439c982d9beaba47
zeropointx <tti13smiikas@kamk.fi>
2015-12-13 13:32:23 +0200

    Everything in stage4 can be enabled with one script call

---
+++ LudumDare34/Assets/_Scripts/Stage4/EnableStage4.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableStage4 : MonoBehaviour {
+    public GameObject enemyHandlerPrefab;
+    public GameObject mapPrefab;
+	// Use this for initialization
+	void Start () {
+        Enable();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Enable()
+    {
+        GameObject map = GameObject.Instantiate(mapPrefab);
+        map.name = "Map";
+        GameObject enemy = GameObject.Instantiate(enemyHandlerPrefab);
+
+    }
+}
