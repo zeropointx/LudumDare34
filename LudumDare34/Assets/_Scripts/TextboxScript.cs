56bd75d5a9de8dfd5a3760c927ac82dabddc2a60
Zegram <Joikkiz@hotmail.com>
2015-12-14 14:51:25 +0200

    pick-a-tron animated & redone.

--- LudumDare34/Assets/_Scripts/TextboxScript.cs
+++ LudumDare34/Assets/_Scripts/TextboxScript.cs
@@ -6,7 +6,7 @@ public class TextboxScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        Time.timeScale = 0;
+        //Time.timeScale = 0;
 	}
 	
 	// Update is called once per frame
