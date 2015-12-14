cd57c70d0f10c917693f7e1b9bd88dfd042f7447
Zegram <Joikkiz@hotmail.com>
2015-12-14 14:16:49 +0200

    added font and some textbox stuffs

---
+++ LudumDare34/Assets/_Scripts/TextboxChanger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextboxChanger : MonoBehaviour {
+
+    public GameObject Textbox1;
+    public GameObject Textbox2;
+    public GameObject Textbox3;
+    public GameObject Textbox4;
+
+	// Use this for initialization
+    void Awake()
+    {
+        setBox2(false);
+        setBox3(false);
+        setBox4(false);
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {	
+	}
+
+    public void setBox1(bool state)
+    {
+        Textbox1.SetActive(state);
+    }
+
+    public void setBox2(bool state)
+    {
+        Textbox2.SetActive(state);
+    }
+
+    public void setBox3(bool state)
+    {
+        Textbox3.SetActive(state);
+    }
+
+    public void setBox4(bool state)
+    {
+        Textbox4.SetActive(state);
+    }
+}
+
---
+++ LudumDare34/Assets/_Scripts/TextboxScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextboxScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
